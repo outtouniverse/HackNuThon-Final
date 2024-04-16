f841512ab38796d4e7302d220d9f08298b1095c1
TulipJani <tkjani20@gmail.com>
2024-04-17 00:42:54 +0530

    assetsPageAdded

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const collection=require('../models/config');
-const Project=require('../models/user');
+const express = require("express");
+const bodyParser = require("body-parser");
+const collection = require("../models/config");
+const Project = require("../models/user");
 const app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('register');
+app.get("/", (req, res) => {
+  res.render("register");
 });
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     const { email, password, confirmpassword } = req.body;
 
@@ -23,51 +23,52 @@ app.post('/', async (req, res) => {
 
     // Check if password and confirm password match
     if (password !== confirmpassword) {
-      return res.status(400).json({ error: 'Passwords do not match' });
+      return res.status(400).json({ error: "Passwords do not match" });
     }
 
     // Create a new user
     const newUser = new collection({
       email,
-      password
+      password,
     });
     await newUser.save();
     res.redirect("/dash");
   } catch (err) {
-    console.error('Error saving user:', err);
-    res.status(500).json({ error: 'Failed to save user to database' });
+    console.error("Error saving user:", err);
+    res.status(500).json({ error: "Failed to save user to database" });
   }
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
-app.post('/login',async(req,res)=>{
-  try{
-    const check=await collection.findOne({email:req.body.email});
-    if(!check){
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ email: req.body.email });
+    if (!check) {
       res.send("email not found");
     }
-    const ispassword=await collection.findOne({password:req.body.password});
-    if(ispassword){
+    const ispassword = await collection.findOne({
+      password: req.body.password,
+    });
+    if (ispassword) {
       res.redirect("/dash");
-    }else{
+    } else {
       req.send("wrong password");
     }
-    
-  }catch{
+  } catch {
     req.send("wrong details");
   }
 });
 
-app.get('/dash', (req, res) => {
-  res.render('home');
-})
-app.get('/assign',async (req,res)=>{
-  const projects = await Project.find();
-  res.render('assign', { projects });
+app.get("/dash", (req, res) => {
+  res.render("home");
 });
-app.post('/assign', async (req, res) => {
+app.get("/assign", async (req, res) => {
+  const projects = await Project.find();
+  res.render("assign", { projects });
+});
+app.post("/assign", async (req, res) => {
   try {
     const { title, description, startDate, endDate } = req.body;
 
@@ -81,16 +82,19 @@ app.post('/assign', async (req, res) => {
     // Save the project to the database
     await project.save();
     const projects = await Project.find();
-    res.redirect('/assign');
+    res.redirect("/assign");
   } catch (error) {
-    console.error('Error saving project:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error saving project:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
-app.get('/members',(req,res)=>{
-  res.render('member');
+app.get("/members", (req, res) => {
+  res.render("member");
 });
 
+app.get("/assets", (req, res) => {
+  res.render("assets");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
